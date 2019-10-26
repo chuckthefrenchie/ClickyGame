c133c8747e386d21ac28f0f88de8dc1f068dee62
Daria Korbysheva <korbyshevadaria@gmail.com>
2019-10-26 09:43:21 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ClickyGame
+
+A memory game with React
+
+Click on an image to earn points, but don't click on any more than once!
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import "./style.css";
+
+function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li>Clicky Game</li>
+            <li>Click an Image to Begin</li>
+            <li>Score:</li>
+        </ul>
+    </nav>
+  );
+}
+
+
+export default Navbar;
